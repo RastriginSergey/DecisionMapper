3205dd118ad3959583ca3810d6dcb224c19cfeeb
Sergey Rastrigin <s.rastrigin@gmail.com>
2017-05-05 09:34:17 +0300

    add html engine

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const path = require('path');
+const engine = require('consolidate');
+
+app.set('views', __dirname + '/views');
+app.engine('html', engine.mustache);
+app.set('view engine', 'html');
 
 app.use(express.static(path.resolve(__dirname, 'build')));
 
 app.get('*', (req, res, next) => {
-    res.render('index.html');
+    res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
